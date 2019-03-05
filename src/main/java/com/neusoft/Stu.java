b99175ab9ac88f76b4ac46b33d53f11b9f057063
‘WYQ77’ <rikkirikki@163.com>
2019-03-05 14:39:42 +0800

    This is WYQ's correct

--- src/main/java/com/neusoft/Stu.java
+++
@@ -1,7 +0,0 @@
-package com.neusoft;
-
-public class Stu {
-    public static void main(String[] args) {
-        System.out.println("这是一个小组任务");
-    }
-}
---
+++ src/main/java/com/neusoft/WYQ.java
@@ -0,0 +1,7 @@
+package com.neusoft;
+
+public class WYQ {
+    public static void main(String[] args) {
+        System.out.println("这是一个小组任务啦啦啦");
+    }
+}
